ac5f0281157917ace814cd72a60cbcc7ad73c91c
Kanjula Gunathilaka <ksgunathilaka071@gmail.com>
2023-05-18 00:05:26 +1200

    Updated login details and dependencies

--- MarsQA-1/SpecflowPages/Helpers/ConstantHelpers.cs
+++ MarsQA-1/SpecflowPages/Helpers/ConstantHelpers.cs
@@ -12,12 +12,12 @@ namespace MarsQA_1.Helpers
         public static string Url = "http://localhost:5000";
 
         //ScreenshotPath
-        public static string ScreenshotPath = "";
+        public static string ScreenshotPath = "C:\\AAAindustryConnect\\internship\\onboarding.specflow\\MarsQA-1\\TestReports\\Screenshots\\";
 
         //ExtentReportsPath
-        public static string ReportsPath = "";
+        public static string ReportsPath = "C:\\AAAindustryConnect\\internship\\onboarding.specflow\\MarsQA-1\\TestReports\\Test_files\\";
 
         //ReportXML Path
-        public static string ReportXMLPath = "";
+        public static string ReportXMLPath = "C:\\AAAindustryConnect\\internship\\onboarding.specflow\\MarsQA-1\\TestReports\\Test_files\\";
     }
 }
