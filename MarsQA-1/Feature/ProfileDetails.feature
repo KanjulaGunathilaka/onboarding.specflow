0cfc6fb9fe092058d6b763aab1e177c2fa5603eb
Kanjula Gunathilaka <ksgunathilaka071@gmail.com>
2023-05-23 21:27:57 +1200

    shared profile details

--- MarsQA-1/Feature/ProfileDetails.feature
+++ MarsQA-1/Feature/ProfileDetails.feature
@@ -13,3 +13,21 @@ Scenario: Seller Add Sellers profile details
 	And Seller enter education details
 	And Seller enter Certification details
 	Then Seller able to add profile details successfully
+
+Scenario: Seller share Sellers profile details
+Given Seller logged in to the website
+When Seller is navigate to share skill page
+And Seller enter "Valid" Title
+And Seller enter "Valid" Description
+And Seller enter "Valid" Description
+And Seller enter tags 
+And Seller select Service type
+And Seller select Location type
+And Seller select available start date and end date
+And Seller select available start date and end date
+And Seller choose skill trade
+And Seller add new tags for skill exchange
+And Seller upload work samples
+And Seller Active services
+And Seller click save button
+Then Seller is able to save share skill page succussfully
---
+++ MarsQA-1/StepDefinitions/SharePageDetailsStepDefinitions.cs
@@ -0,0 +1,87 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace MarsQA_1.StepDefinitions
+{
+    [Binding]
+    public class SharePageDetailsStepDefinitions
+    {
+        [When(@"Seller is navigate to share skill page")]
+        public void WhenSellerIsNavigateToShareSkillPage()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"Seller enter ""([^""]*)"" Title")]
+        public void WhenSellerEnterTitle(string valid)
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"Seller enter ""([^""]*)"" Description")]
+        public void WhenSellerEnterDescription(string valid)
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"Seller enter tags")]
+        public void WhenSellerEnterTags()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"Seller select Service type")]
+        public void WhenSellerSelectServiceType()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"Seller select Location type")]
+        public void WhenSellerSelectLocationType()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"Seller select available start date and end date")]
+        public void WhenSellerSelectAvailableStartDateAndEndDate()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"Seller choose skill trade")]
+        public void WhenSellerChooseSkillTrade()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"Seller add new tags for skill exchange")]
+        public void WhenSellerAddNewTagsForSkillExchange()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"Seller upload work samples")]
+        public void WhenSellerUploadWorkSamples()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"Seller Active services")]
+        public void WhenSellerActiveServices()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"Seller click save button")]
+        public void WhenSellerClickSaveButton()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"Seller is able to save share skill page succussfully")]
+        public void ThenSellerIsAbleToSaveShareSkillPageSuccussfully()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
